eab96396179848b8883b99046c72fbb5a04b41b4
Mohammad Sajjad Hossain <mohammadsajjad24385@gmail.com>
2021-07-31 23:39:33 +0600

    create Model for User, Profile,Post,Comment

---
+++ models/Comment.js
@@ -0,0 +1,41 @@
+const {Schema, model} = require('mongoose')
+const User = require("./User");
+const commentSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: User,
+        required: true
+    },
+    post: {
+        type: Schema.Types.ObjectId,
+        ref: User,
+        required: true
+    },
+    body: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    replies: [
+        {
+            body: {
+                type: String,
+                required: true
+            },
+            user: {
+                type: Schema.Types.ObjectId,
+                ref: User,
+                required: true
+            },
+            createdAt: {
+                type: Date,
+                default: new Date()
+            }
+        }
+    ]
+}, {
+    timestamps: true
+})
+
+const Comment = model('Comment', commentSchema)
+module.exports = Comment
