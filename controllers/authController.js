7823c2f4e907cd5584bd9a47e8446c21d6b06779
Mohammad Sajjad Hossain <mohammadsajjad24385@gmail.com>
2021-08-03 03:55:33 +0600

    add existing value append in input field

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,61 +1,73 @@
 const User = require('../models/User')
 const bcrypt = require('bcrypt')
-const {validationResult} = require("express-validator");
+const {validationResult, body} = require("express-validator");
 const errorFormatter = require('../utils/validationErrorFormatter')
 
 exports.signupGet = (req, res, next) => {
-    res.render('pages/auth/signup', {title: 'Create A New Account', error: {}})
+    res.render('pages/auth/signup',
+        {
+            title: 'Create A New Account',
+            error: {},
+            existingValue: {}
+        })
 }
 exports.signupPost = async (req, res, next) => {
+    let {username, email, password, confirmPassword} = req.body
     let errors = validationResult(req).formatWith(errorFormatter)
     if (!errors.isEmpty()) {
         console.log(errors.mapped())
-        return res.render('pages/auth/signup', {title: 'Create A New Account', error: errors.mapped()})
+        return res.render('pages/auth/signup', {
+            title: 'Create A New Account',
+            error: errors.mapped(),
+            existingValue: {
+                username, email, password
+            }
+        })
     }
-        try {
-            let {username, email, password, confirmPassword} = req.body
-            let hashPassword = await bcrypt.hash(password, 11)
-            let user = new User({
-                username,
-                email,
-                password: hashPassword
+    try {
+        let {username, email, password, confirmPassword} = req.body
+        let hashPassword = await bcrypt.hash(password, 11)
+        let user = new User({
+            username,
+            email,
+            password: hashPassword
+        })
+        let createdUser = await user.save()
+        console.log('User Created Successfully', createdUser)
+        res.render('pages/auth/signup', {title: 'Create A New Account'})
+    } catch (e) {
+
+    }
+
+}
+
+exports.loginGet = (req, res, next) => {
+    res.render('pages/auth/login', {title: 'Login Your Account'})
+}
+exports.loginPost = async (req, res, next) => {
+    let {email, password} = req.body
+    try {
+        let user = await User.findOne({email})
+        if (!user) {
+            return res.json({
+                message: 'Invalid Credentials'
             })
-            let createdUser = await user.save()
-            console.log('User Created Successfully', createdUser)
-            res.render('pages/auth/signup', {title: 'Create A New Account'})
-        } catch (e) {
-
         }
+        let passwordMatch = await bcrypt.compare(password, user.password)
 
-    }
-
-    exports.loginGet = (req, res, next) => {
+        if (!passwordMatch) {
+            return res.json({
+                message: 'Invalid Credentials'
+            })
+        }
+        console.log('Successfully Logged In', user)
         res.render('pages/auth/login', {title: 'Login Your Account'})
-    }
-    exports.loginPost = async (req, res, next) => {
-        let {email, password} = req.body
-        try {
-            let user = await User.findOne({email})
-            if (!user) {
-                return res.json({
-                    message: 'Invalid Credentials'
-                })
-            }
-            let passwordMatch = await bcrypt.compare(password, user.password)
-
-            if (!passwordMatch) {
-                return res.json({
-                    message: 'Invalid Credentials'
-                })
-            }
-            console.log('Successfully Logged In', user)
-            res.render('pages/auth/login', {title: 'Login Your Account'})
 
 
-        } catch (e) {
-
-        }
-    }
-    exports.logout = (req, res, next) => {
+    } catch (e) {
 
     }
+}
+exports.logout = (req, res, next) => {
+
+}
