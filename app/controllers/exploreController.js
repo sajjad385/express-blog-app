aef4dd0b6f0f88032c1889734e72c9d8080e8865
Mohammad Sajjad Hossain <mohammadsajjad24385@gmail.com>
2022-07-12 00:02:38 +0600

    add pagination

--- app/controllers/exploreController.js
+++ app/controllers/exploreController.js
@@ -2,6 +2,7 @@ const moment = require('moment')
 
 const Flash = require("../../utils/Flash");
 const Post = require('../models/Post')
+const Profile = require("../models/Profile");
 const generateDate = days => {
     let date = moment().subtract(days, 'days')
     return date.toDate()
@@ -46,17 +47,35 @@ const generateFilterObject = filter => {
 }
 exports.index = async (req, res, next) => {
     let filter = req.query.filter || 'latest'
-    let {filterObj, order} = generateFilterObject(filter)
+    let currentPage = parseInt(req.query.page) || 1
+    let itemPerPage = parseInt(req.query.limit) || 2
+    let {filterObj, order} = generateFilterObject(filter.toLowerCase())
     try {
         let posts = await Post.find(filterObj)
             .populate('author', 'username')
             .sort(order === 1 ? '-createdAt' : 'createdAt')
-
+            .skip((itemPerPage * currentPage) - itemPerPage)
+            .limit(itemPerPage)
+        let totalPost = await Post.countDocuments()
+        let totalPage = totalPost / itemPerPage
+        let bookmarks = []
+        if (req.user) {
+            let profile = await Profile.findOne({user: req.user._id})
+            if (profile) {
+                bookmarks = profile.bookmarks
+            }
+        }
         res.render('pages/explorer/index', {
             title: 'Explore All Posts',
             filter: filter,
             flashMessage: Flash.getMessage(req),
-            posts
+            posts,
+            itemPerPage,
+            currentPage,
+            totalPost,
+            totalPage,
+            bookmarks
+
         })
     } catch (e) {
         next(e)
